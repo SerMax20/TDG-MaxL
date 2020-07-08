8f0a3a1e22501392c53d44489ea92a8b731e9f15
SerMax20 <67185027+SerMax20@users.noreply.github.com>
2020-07-08 07:08:19 +0000

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -162,7 +162,7 @@ async def upload_to_gdrive(file_upload, message, messa_ge, g_id):
             button.append([pyrogram.InlineKeyboardButton(text="👉INDEX URL😉", url=f"{tam_link}")])
         button_markup = pyrogram.InlineKeyboardMarkup(button)
         await asyncio.sleep(EDIT_SLEEP_TIME_OUT)
-        await messa_ge.reply_text(f"🤖: {file_upload} <b>has been Uploaded successfully to your Cloud</b> <a href='tg://user?id={g_id}'>😌</a>\n\n<i><b>🛑YOU MUST BE JOIN TEAM DRIVE FOR ACCESS LINK😂 👉 <a href=\"https://groups.google.com/forum/m/#!forum/maxxleech_uplpad\">JOIN_TEAM_DRIVE</a>", reply_markup=button_markup, disable_web_page_preview=True)
+        await messa_ge.reply_text(f"🤖: {file_upload} <b>has been Uploaded successfully to your Cloud</b> <a href='tg://user?id={g_id}'>😌</a>\n\n<i><b>🛑YOU MUST BE JOIN TEAM DRIVE FOR ACCESS LINK😂 👉 <a href=\"https://groups.google.com/forum/m/#!forum/\">JOIN_TEAM_DRIVE</a>", reply_markup=button_markup, disable_web_page_preview=True)
         #await message.edit_text(f"""🤖: {file_upload} has been Uploaded successfully to your cloud 🤒\n\n☁️ Cloud URL:  <a href="{gau_link}">FileLink</a>\nℹ️ Direct URL:  <a href="{tam_link}">IndexLink</a>""")
         os.remove(file_upload)
         await del_it.delete()
